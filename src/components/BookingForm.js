e18717fd55778bdfed256a85b0aaaafdfb5e4621
LudivineLeLan <lelanludivine@gmail.com>
2025-12-05 12:07:18 +0100

    added mail input + updated html/favicon

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 function BookingForm({ slot, onBooked, onCancel }) {
   const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -12,13 +14,18 @@ function BookingForm({ slot, onBooked, onCancel }) {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           userName: name,
+          userEmail: email,
           slotId: slot.id
         })
       });
 
       if (!response.ok) throw new Error("Impossible de réserver le créneau");
-
-      onBooked();
+      setSuccessMessage("Réservation validée ! 🎶");
+      setName("");
+      setEmail("");
+      setTimeout(() => {
+        onBooked();
+      }, 2500);
     } catch (error) {
       console.error(error);
       alert(error.message);
@@ -30,9 +37,12 @@ function BookingForm({ slot, onBooked, onCancel }) {
       <h3>Réserver le créneau : {new Date(slot.date).toLocaleString()}</h3>
       <input
         type="text" placeholder="Votre nom" value={name} onChange={event => setName(event.target.value)} required />
+      <input type="email" placeholder="Votre email" value={email} onChange={event => setEmail(event.target.value)} required />
       <button type="submit" className="button-booking">Réserver</button>
       <button type="button" className="button-cancel" onClick={onCancel}>Annuler</button>
+      {successMessage && <p className="success-message">{successMessage}</p>}
     </form>
+
   );
 }
 
